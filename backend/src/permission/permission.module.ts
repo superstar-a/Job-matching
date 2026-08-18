c128cd0b724dce28a64894b6f59e716eba2f027c
DuongDaiPhat <duongphat521@gmail.com>
2026-08-18 14:43:55 +0700

    fix(api): added jwt dependencies for permissions and roles modules

--- backend/src/permission/permission.module.ts
+++ backend/src/permission/permission.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PermissionService } from './permission.service';
 import { PermissionController } from './permission.controller';
 import { DatabaseModule } from 'src/database/database.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, UsersModule],
   controllers: [PermissionController],
   providers: [PermissionService],
   exports: [PermissionService]
--- backend/src/roles/roles.module.ts
+++ backend/src/roles/roles.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { RolesController } from './roles.controller';
 import { DatabaseModule } from 'src/database/database.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, UsersModule],
   controllers: [RolesController],
   providers: [RolesService],
   exports: [RolesService],
